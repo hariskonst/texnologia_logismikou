49c667d39de2e681b97f9a3acf8cca5cf0217ade
Christidis <loukalex02@yahoo.com>
2025-05-20 17:18:33 +0300

    Initial commit 1

---
+++ app/src/main/java/com/example/myapplication/ManagerHomeScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class ManagerHomeScreenActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.manager_home_scr)
+
+        val secondButton = findViewById<ImageButton>(R.id.UploadReceiptButton)
+        secondButton.setOnClickListener {
+            val intent = Intent(this, ReceiptManagerScrActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
