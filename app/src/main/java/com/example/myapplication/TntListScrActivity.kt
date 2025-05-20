38446908ca28037b41027cbf9aeb9a001a663791
Haris <harryskonst@gmail.com>
2025-05-20 21:24:04 +0300

    tnt list screen

--- app/src/main/java/com/example/myapplication/TntListScrActivity.kt
+++ app/src/main/java/com/example/myapplication/TntListScrActivity.kt
@@ -1,29 +1,20 @@
 package com.example.myapplication
 
-
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.ViewCompat
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TenapMngrScrActivity : AppCompatActivity() {
+class TntListScrActivity : AppCompatActivity(){
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.tenap_mngr_scr)
-
-        val removetnt = findViewById<Button>(R.id.remove_tnt)
-        removetnt.setOnClickListener {
-            val intent = Intent(this, TntListScrActivity::class.java)
-            startActivity(intent)
-
-        }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tenap_manager)) { v, insets ->
+        setContentView(R.layout.tnt_list_scr)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tnt_list)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
