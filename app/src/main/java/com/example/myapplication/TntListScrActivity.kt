85040338ea00cf6c00220b2c4bbb5fc05a1eacb4
Haris <harryskonst@gmail.com>
2025-05-21 19:33:57 +0300

    TntListScrActivity.kt mngr_home_scr.xml

--- app/src/main/java/com/example/myapplication/TntListScrActivity.kt
+++ app/src/main/java/com/example/myapplication/TntListScrActivity.kt
@@ -14,10 +14,5 @@ class TntListScrActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tnt_list_scr)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tnt_list)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
-}
