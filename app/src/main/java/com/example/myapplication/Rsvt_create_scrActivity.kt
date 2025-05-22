49cea35419c8da9b8f1accbae98cc9f7fb4259d6
DimitrisEfthimakis <dimitrisefthimakis@gmail.com>
2025-05-23 02:40:14 +0300

    Reservation case screens

---
+++ app/src/main/java/com/example/myapplication/Rsvt_create_scrActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Rsvt_create_scrActivity: AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.rsvt_scr)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Reservation)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/myapplication/TenantHomeScreenActivity.kt
+++ app/src/main/java/com/example/myapplication/TenantHomeScreenActivity.kt
@@ -1,14 +1,15 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.button.MaterialButton
 import android.widget.Button
 import android.widget.ImageButton
 
 
 class TenantHomeScreenActivity : AppCompatActivity() {
+    @SuppressLint("CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tenant_home_scr)
@@ -19,9 +20,15 @@ class TenantHomeScreenActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val paymentBtn = findViewById<ImageButton>(R.id.imageButton11)
-        paymentBtn.setOnClickListener {
-            val intent = Intent(this, Tnt_Payment_ScrActivity::class.java)
+        val RsvtBtn = findViewById<Button>(R.id.buttonReservations)
+        RsvtBtn.setOnClickListener {
+            val intent = Intent(this, Rsvt_create_scrActivity::class.java)
+            startActivity(intent)
+        }
+
+        val reserve = findViewById<Button>(R.id.LostFound)
+        reserve.setOnClickListener {
+            val intent = Intent(this, Tnt_LostFound_ScrActivity::class.java)
             startActivity(intent)
         }
 
@@ -30,7 +37,5 @@ class TenantHomeScreenActivity : AppCompatActivity() {
             val intent = Intent(this, Tenant_Poll_ScrActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
