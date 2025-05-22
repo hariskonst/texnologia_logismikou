fa59dcb6d6fab51eac8b7f188ad45392c34470c5
Haris <harryskonst@gmail.com>
2025-05-22 11:37:28 +0300

    remove tenant- go to add screen

---
+++ app/src/main/java/com/example/myapplication/NewTntAddScrActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class NewTntAddScrActivity : AppCompatActivity(){
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.new_tnt_add_scr)
+    }
+}
--- app/src/main/java/com/example/myapplication/TntListScrActivity.kt
+++ app/src/main/java/com/example/myapplication/TntListScrActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,5 +16,11 @@ class TntListScrActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.tnt_list_scr)
+
+        val confirm = findViewById<Button>(R.id.gotoadd)
+        confirm.setOnClickListener {
+            val intent = Intent(this, NewTntAddScrActivity::class.java)
+            startActivity(intent)
+            }
         }
     }
