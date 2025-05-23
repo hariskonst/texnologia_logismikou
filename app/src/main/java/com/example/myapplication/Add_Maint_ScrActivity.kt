e6bcbb5f751716b028b4c150942e36adcd29fa70
Haris <harryskonst@gmail.com>
2025-05-23 11:13:43 +0300

    diorthwseis

--- app/src/main/java/com/example/myapplication/Add_Maint_ScrActivity.kt
+++ app/src/main/java/com/example/myapplication/Add_Maint_ScrActivity.kt
@@ -1,31 +1,24 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Maint_ScrActivity : AppCompatActivity() {
+class Add_Maint_ScrActivity : AppCompatActivity() {
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.maint_scr)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.maintenancescr)) { v, insets ->
+        setContentView(R.layout.add_maint_scr)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.maintscr)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val new = findViewById<Button>(R.id.newMaintenance)
-        new.setOnClickListener {
-            val intent = Intent(this, Add_Maint_ScrActivity::class.java)
-            startActivity(intent)
-        }
     }
+
 }
