a5fff9751ff680f2919ee43e34144e8ff64b5f63
Christidis <loukalex02@yahoo.com>
2025-05-25 21:43:23 +0300

    commit

--- app/src/main/java/com/example/myapplication/Mngr_Poll_ScrActivity.kt
+++ app/src/main/java/com/example/myapplication/Mngr_Poll_ScrActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +20,14 @@ class Mngr_Poll_ScrActivity : AppCompatActivity(){
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+
+
+
     }
+
+
 
 }
